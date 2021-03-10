5154fd0af4b4ea0ab8e9b2cdc2b403d1392ed007
Liam Hilton-Green <liamhg1@yahoo.com>
2021-03-10 15:11:29 -0600

    Separated ProcessUtility into package

---
+++ PrivacyHub/WindowsProcessUtilityPackage/OBJECT_NAME_INFORMATION.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PrivacyHub.WindowsProcessUtility
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct OBJECT_NAME_INFORMATION
+    { // Information Class 1
+        public UNICODE_STRING Name;
+    }
+}
---
+++ PrivacyHub/WindowsProcessUtilityPackage/SYSTEM_HANDLE_INFORMATION.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PrivacyHub.WindowsProcessUtility
+{
+    /// <summary>
+    /// https://www.geoffchappell.com/studies/windows/km/ntoskrnl/api/ex/sysinfo/handle_table_entry.htm?ts=0,242
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct SYSTEM_HANDLE_INFORMATION
+    { // Information Class 16
+        public ushort ProcessID;
+        public ushort CreatorBackTrackIndex;
+        public byte ObjectType;
+        public byte HandleAttribute;
+        public ushort Handle;
+        public IntPtr Object_Pointer;
+        public IntPtr AccessMask;
+    }
+}
